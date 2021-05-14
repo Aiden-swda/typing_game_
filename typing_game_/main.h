0f6b45b192f21b1b586edbd3fb25a254cc104b91
진률 백 <wii070@naver.com>
2021-05-14 20:12:56 +0900

    색깔 바꾸는 함수 추가, 첫 화면 일부 수정

--- typing_game_/main.h
+++ typing_game_/main.h
@@ -5,6 +5,7 @@
 
 void first();
 void gotoxy(int x, int y);
+void ColorSet(int color_number);
 
 #define COLS 120
-#define LINES 40
+#define LINES 45
--- typing_game_/story.c
+++ typing_game_/story.c
@@ -9,19 +9,24 @@ void first()  //첫 화면 출력하는 함수
 	{
 		printf("*");
 	}
-	gotoxy(COLS / 2 - 12, LINES / 2 - 4);
-	printf("건덕이 잡기");
-	gotoxy(COLS / 2 - 12, LINES / 2 - 3);
+	gotoxy(COLS / 2 - 12, LINES / 2 - 6);
+	printf("  ");
+	ColorSet(94);
+	printf("<건덕이 잡기>");
+	ColorSet(7);
+	gotoxy(COLS / 2 - 12, LINES / 2 - 5);
 	printf("우리 조 이름");
-	gotoxy(COLS / 2 - 12, LINES / 2 - 2);
+	gotoxy(COLS / 2 - 12, LINES / 2 - 3);
 	printf("1. 게임 시작");
-	gotoxy(COLS / 2 - 12, LINES / 2 - 1);
+	gotoxy(COLS / 2 - 12, LINES / 2 - 2);
 	printf("2. 도움말(첫판 필수)");
-	gotoxy(COLS / 2 - 12, LINES / 2);
+	gotoxy(COLS / 2 - 12, LINES / 2 - 1);
 	printf("3. 점수 보기");
-	gotoxy(COLS / 2 - 12, LINES / 2 + 1);
+	gotoxy(COLS / 2 - 12, LINES / 2);
 	printf("4. 게임종료\n");
-	gotoxy(0, LINES);         //마지막 줄에 별 출력하려했는데 안되네.. 뭐지
+	gotoxy(COLS / 2 - 12, LINES / 2 + 2);
+	printf("입력:");
+	gotoxy(0, LINES-5);
 	for (i = 1; i <= COLS; i++)
 	{
 		printf("*");
@@ -32,4 +37,9 @@ void gotoxy(int x, int y)  //요게 이제 커서 위치 조정
 {
 	COORD Cur = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+}
+void ColorSet(int color_number)  //글자 색, 배경 색 바꾸는 함수
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(handle, color_number);
 }
