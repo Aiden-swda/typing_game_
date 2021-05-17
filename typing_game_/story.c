70c36f42fe566c7846889176f7ac7cc86fcc33d8
yunuo468@gmail.com <yunuo468@gmail.com>
2021-05-17 18:00:58 +0900

    주석 추가(스토리 출력 함수 진행방식)

--- typing_game_/story.c
+++ typing_game_/story.c
@@ -14,7 +14,7 @@ void ColorSet(int color_number)  //글자 색, 배경 색 바꾸는 함수
 	SetConsoleTextAttribute(handle, color_number);
 }
 
-void texts(char text[]) // 텍스트 출력시간
+void texts(char text[]) // 텍스트 출력시간 설정
 {
 	for (int i = 0; i < strlen(text); i++)
 	{
@@ -23,52 +23,17 @@ void texts(char text[]) // 텍스트 출력시간
 	}
 }
 
-void story() // 스토리 출력 방식 예시 (나중에 변경해서 사용)
+void story() // 스토리 출력 방식 예시 (내가 더 최적화를 해볼게 아직 미완)
+// 실행방식 : 텍스트창 내부로 (x,y)좌표 설정 -> 배열에 텍스트 저장 -> texts함수로 출력조절 -> screen_s함수로 좌표변경 or 텍스트창 초기화
 {
 	int story_x = 2;
 	int story_y = LINES - 14;
 
 	gotoxy(story_x, story_y);
-	char story1[] = "[system : 게임이 시작됩니다.]";
-	texts(story1);
+	char story1[] = "[system : 게임이 시작됩니다.]";  // 배열로 출력할 텍스트 선언
+	texts(story1); // texts함수로 텍스트 출력 천천히하기
 	Sleep(1000);
-	screen_s(&story_y);
-
-	gotoxy(story_x, story_y);
-	char story2[] = "[system : 이번 난이도를 클리어하셨습니다.]";
-	texts(story2);
-	Sleep(1000);
-	screen_s(&story_y);
-
-	gotoxy(story_x, story_y);
-	char story3[] = "[system : 체력이 1 소모되었습니다.]";
-	texts(story3);
-	Sleep(1000);
-	screen_s(&story_y);
-
-	gotoxy(story_x, story_y);
-	char story4[] = "[system : 콤보 성공! 추가 점수를 획득합니다.]";
-	texts(story4);
-	Sleep(1000);
-	screen_s(&story_y);
-
-	gotoxy(story_x, story_y);
-	char story5[] = "[system : 다음 난이도로 이동합니다.]";
-	texts(story5);
-	Sleep(1000);
-	screen_s(&story_y);
-
-	gotoxy(story_x, story_y);
-	char story6[] = "[system : 축하드립니다! 게임을 클리어하셨습니다!]";
-	texts(story6);
-	Sleep(1000);
-	screen_s(&story_y);
-
-	gotoxy(story_x, story_y);
-	char story7[] = "[system : 새로운 창에서 스토리 출력]";
-	texts(story7);
-	Sleep(1000);
-	screen_s(&story_y);
+	screen_s(&story_y);  // screen_s함수로 story_y값 전달 후 조건에 따라 story_y값 변경
 
 	return 0;
 }
