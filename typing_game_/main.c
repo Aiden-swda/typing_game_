f6d46bb0b769f72bb9298be6af9a3fc8a0c4dcdc
SIKU-KR <peter012677@naver.com>
2021-05-26 13:14:11 +0900

    05/26 박범식 새로운 출력함수 생성 print_letter 메인함수 작성 단어 읽기 진률이가 작성해준걸로 교체

--- typing_game_/main.c
+++ typing_game_/main.c
@@ -2,16 +2,45 @@
 
 int main()
 {
+	int input; //사용자 입력 (메뉴선택)
+	//settings
 	system("mode con cols=120 lines=45");
 	srand(time(NULL));
-	
-	//story();
-
-	//first();
 	read_file();
 	
-	for(int i = 0; i = 5; i++)
-	game_run();
+	system("pause");
+	system("cls");
+	
+	//intro
+	first();
+	screen();
+	print_letter("메뉴를 입력하세요 : ");
+	input = input_menu();
+	switch (input)
+	{
+	case 1:
+		//게임실행함수
+		screen();
+		print_letter("게임 실행 함수");
+		break;
+	case 2:
+		//도움말 보기
+		screen();
+		print_letter("도움말 보기");
+		break;
+	case 3:
+		screen();
+		printf("총 점수 : %d", totalScore);
+		break;
+	case 4:
+		return 0;
+	}
+
+	system("pause");
+	system("cls");
+
+	//game run
+	
 
 	return 0;
 }
