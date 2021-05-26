f36889dfd8de29c9f06aec50aa5ef193a37c2d32
SIKU-KR <peter012677@naver.com>
2021-05-26 15:58:06 +0900

    코드병합 111

--- typing_game_/LDM_test_3.c
+++ typing_game_/LDM_test_3.c
@@ -1,16 +1,15 @@
-﻿//�̴ٹ� �׽�Ʈ ����3
+﻿//이다민 테스트 파일3
 #include "main.h"
 
 int game_run()
 {
 
 	char input[15]; //입력받을 단어 배열
-	int a; //���� ���� ���� ����
-	int check; //���ڿ� ���� �� ��� ����
-	int score = 0; //���� ����
-	int heart = 5; //���� ��� ����
-	double result; //�ð����� �����ϱ� ���� ����
-	int wordLength;
+	int a; //랜덤 숫자 받을 변수
+	int check; //문자열 비교한 값 담는 변수
+	int score = 0; //점수 변수
+	int heart = 5; //남은 목숨 변수
+	double result; //시간값을 저장하기 위한 변수
 
 	int b = 0;
 
@@ -19,12 +18,12 @@ int game_run()
 
 	int wordLength;
 
-	//system("mode con cols=120 lines=45"); //�ܼ�â ũ��
-	system("title �Ǵ��̸� ��ƶ�!"); //�ܼ�â ����
-	system("cls"); // �ܼ�â �ʱ�ȭ
+	//system("mode con cols=120 lines=45"); //콘솔창 크기
+	system("title 건덕이를 잡아라!"); //콘솔창 제목
+	system("cls"); // 콘솔창 초기화
 	//srand(time(NULL));
 	gotoxy(110, 0);
-	printf("������\n"); //ü�¹� �׽�Ʈ
+	printf("■■■■■\n"); //체력바 테스트
 
 	srand(time(0));
 	s_time = time(0);
@@ -32,14 +31,14 @@ int game_run()
 	printf("%d", wordLength);
 	gotoxy(COLS / 2 - 5, 3);
 	printf("%s\n", wordBase[a]);
-	
+
 
 	while (1)
 	{
-		
-		//a = get_random_word(); //�迭���� �ܾ� �����ϱ� ���� ���� �ε��� ��
+
+		//a = get_random_word(); //배열에서 단어 추출하기 위한 랜덤 인덱스 값
 		//gotoxy(COLS / 2 - 5, 3);
-		//printf("%s\n", wordBase[a]); //�迭���� ���� ����� �ܾ� ���
+		//printf("%s\n", wordBase[a]); //배열에서 랜덤 추출된 단어 출력
 
 		if (time(0) == s_time + 5)
 		{
@@ -54,23 +53,22 @@ int game_run()
 			printf("%s\n", wordBase[a]);
 			s_time = time(0);
 			j = 0;
-			free(inputArray);
 		}
 		if (_kbhit())
 		{
-			
+
 			ch = _getch();
 			gotoxy(COLS / 2 - 5 + j, 5);
 
 			if (ch == 8)
 			{
-				if (j>0)
+				if (j > 0)
 				{
 					j--;
 					printf("%c", ch);
 				}
-				
-				
+
+
 			}
 			if (ch == 27)
 			{
@@ -79,9 +77,9 @@ int game_run()
 			//printf("%c", ch);
 			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 			{
-				
+
 				printf("%c", ch);
-				inputArray[j] = ch;
+				input[j] = ch;
 				j++;
 				if (j == wordLength)
 				{
@@ -90,11 +88,11 @@ int game_run()
 			}
 		}
 
-		if ( b==1 )
+		if (b == 1)
 		{
 			//printf("%s\n", input);
 			input[wordLength] = 0;
-			for (int i = 0; wordBase[a][i] != 0; i++)  //�迭�� �������� �پ��ִ� \n ����
+			for (int i = 0; wordBase[a][i] != 0; i++)  //배열값 마지막에 붙어있는 \n 제거
 			{
 				if (wordBase[a][i] == '\n')
 				{
@@ -102,25 +100,25 @@ int game_run()
 					break;
 				}
 			}
-			check = strcmp(inputArray, wordBase[a]); //���ڿ� ��
+			check = strcmp(input, wordBase[a]); //문자열 비교
 			//printf("%d", check);
 			b = 0;
 			system("cls");
-			switch (check)  // ���ڿ� �񱳰��� ���� ���
+			switch (check)  // 문자열 비교값에 따른 출력
 			{
 			case 0:
 				gotoxy(COLS / 2 - 5, 10);
-				printf("�¾ҽ��ϴ�!\n");
+				printf("맞았습니다!\n");
 				score = score + 10;
 				break;
 			case 1:
 				gotoxy(COLS / 2 - 5, 10);
-				printf("Ʋ�Ƚ��ϴ�!\n");
+				printf("틀렸습니다!\n");
 				heart = heart - 1;
 				break;
 			case -1:
 				gotoxy(COLS / 2 - 5, 10);
-				printf("Ʋ�Ƚ��ϴ�!\n");
+				printf("틀렸습니다!\n");
 				heart = heart - 1;
 				break;
 			}
@@ -134,33 +132,32 @@ int game_run()
 			printf("%s\n", wordBase[a]);
 			s_time = time(0);
 			j = 0;
-			free(inputArray);
 		}
 
-		if (score >= 50) // ������ ���� ����
+		if (score >= 50) // 점수에 따른 동작
 		{
-			system("cls"); // �ܼ�â �ʱ�ȭ
+			system("cls"); // 콘솔창 초기화
 			gotoxy(COLS / 2 - 10, LINES / 2);
-			printf("1�ܰ踦 Ŭ���� �ϼ̽��ϴ�!");
-			Sleep(2000); //2�� �����
+			printf("1단계를 클리어 하셨습니다!");
+			Sleep(2000); //2초 딜레이
 			return 1;
 			break;
 		}
-		else if (heart == 0) //����� 0�� �Ǿ������� ����
+		else if (heart == 0) //목숨이 0이 되었을때의 동작
 		{
-			system("cls"); // �ܼ�â �ʱ�ȭ
+			system("cls"); // 콘솔창 초기화
 			gotoxy(COLS / 2 - 10, LINES / 2);
-			printf("�Ǵ��̰� Ż���ߴ�!�Ф�\n");
+			printf("건덕이가 탈출했다!ㅠㅠ\n");
 			Sleep(1000);
-			system("cls"); // �ܼ�â �ʱ�ȭ
+			system("cls"); // 콘솔창 초기화
 			gotoxy(COLS / 2 - 10, LINES / 2);
-			printf("����� ����: %d", score);
-			Sleep(1000); //1�� �����
+			printf("당신의 점수: %d", score);
+			Sleep(1000); //1초 딜레이
 
-			system("cls"); // �ܼ�â �ʱ�ȭ
+			system("cls"); // 콘솔창 초기화
 			gotoxy(COLS / 2 - 10, LINES / 2);
-			printf("���ӿ���");
-			Sleep(1000); //1�� �����
+			printf("게임오버");
+			Sleep(1000); //1초 딜레이
 			return 0;
 			break;
 		}
